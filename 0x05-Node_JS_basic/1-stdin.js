d60ed2c17be6729d5a3b6207d5f88afc380a0500
Ustaz07 <shamsutukor@gmail.com>
2024-10-02 04:58:06 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,22 +1,16 @@
-// 1-stdin.js
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Display the welcome message
-console.log("Welcome to Holberton School, what is your name?");
-
-// Listen for input from stdin
-process.stdin.on('data', (input) => {
-  // Trim any trailing newline characters from input
-  const name = input.toString().trim();
-  
-  // Display the user's name
-  console.log(`Your name is: ${name}`);
-  
-  // Close the program after displaying the name
-  process.stdin.pause(); // Pause the input stream
-});
-
-// Handle the end of the process
-process.on('SIGINT', () => {
-  console.log("\nThis important software is now closing");
-  process.exit(); // Exit the process
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
