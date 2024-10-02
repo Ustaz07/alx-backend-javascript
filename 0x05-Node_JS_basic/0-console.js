3c776065918af8b1b0d4e836001bd8b10fed8b07
Ustaz07 <shamsutukor@gmail.com>
2024-10-02 04:11:07 +0000

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,12 @@
+// 0-console.js
+
+/**
+ * Displays a message in STDOUT.
+ * @param {string} message - The message to print.
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+// Export the function for use in other files
+module.exports = displayMessage;
